094fe7e0226cbd952d4a00e41d471dc5e0003bc2
Elizabeth Sinyavin <esinyavin01@gmail.com>
2020-07-17 19:12:24 -0400

    Added mapbox

--- map/views.py
+++ map/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the map index.")
+    mapbox_access_token = 'pk.my_mapbox_access_token'
+    # TODO: move this token to Django settings from an environment variable
+    # found in the Mapbox account settings and getting started instructions
+    # see https://www.mapbox.com/account/ under the "Access tokens" section
+    return render(request, "map/index.html", {
+    'mapbox_access_token': mapbox_access_token
+    })
