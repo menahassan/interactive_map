42d4c28793df392fd6e8d4d56ed557055277dac5
menahassan <menah9@gmail.com>
2020-07-18 22:54:35 -0700

    added all markers

--- map/__init__.py
+++ map/__init__.py
@@ -5,11 +5,12 @@ book = openpyxl.load_workbook('Embassies Consulates and Missions Lat Longs.xlsx'
 sheet = book.active
 
 a = []
-for row in sheet.iter_rows(min_row=2, min_col=1, max_row=30, max_col=15):
+for row in sheet.iter_rows(min_row=2, min_col=1, max_row=276, max_col=15):
     col = []
     for cell in row:
         col.append(cell.value)
     a.append(col)
+print(len(a))
 
 
 #arr[0] is ['Active', 'AF', "Cote d'Ivoire"]
