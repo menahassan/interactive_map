99e5b40e1a6778b33434316cd584cfd8e89d1c1c
menahassan <menah9@gmail.com>
2020-07-22 14:48:10 -0700

    added some of country dropdown code

--- map/__init__.py
+++ map/__init__.py
@@ -5,7 +5,12 @@ book = openpyxl.load_workbook('Embassies Consulates and Missions Lat Longs.xlsx'
 sheet = book.active
 
 a = []
+DICT = {}
 for row in sheet.iter_rows(min_row=2, min_col=1, max_row=276, max_col=15):
+    longlat = {}
+    longlat["long"] = row[14]
+    longlat["lat"]= row[13]
+    DICT[row[2]] = longlat
     col = []
     for cell in row:
         col.append(cell.value)
