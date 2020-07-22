95e426aa3cda782c7a2c210b5afbeb55cc16329e
menahassan <menah9@gmail.com>
2020-07-22 16:30:45 -0700

    added country form

--- map/__init__.py
+++ map/__init__.py
@@ -7,16 +7,16 @@ sheet = book.active
 a = []
 DICT = {}
 for row in sheet.iter_rows(min_row=2, min_col=1, max_row=276, max_col=15):
-    longlat = {}
-    longlat["long"] = row[14]
-    longlat["lat"]= row[13]
-    DICT[row[2]] = longlat
     col = []
     for cell in row:
         col.append(cell.value)
     a.append(col)
 
-
+for row in sheet.iter_rows(min_row=2, max_row = 276, min_col=1, max_col=15, values_only=True):
+    longlat = {}
+    longlat["long"] = row[14]
+    longlat["lat"]= row[13]
+    DICT[row[2]] = longlat
 #arr[0] is ['Active', 'AF', "Cote d'Ivoire"]
 #arr[0][0] is 'Active'
 
--- map/views.py
+++ map/views.py
@@ -1,30 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from .forms import CountriesForm
 from map import a
+from map import DICT
 import json
 
 # Create your views here.
 def index(request):
     mapbox_access_token = 'pk.my_mapbox_access_token'
     embassy_list = json.dumps(a)
-    if "country" not in request.session:
-        request.session["country"] = ""
-    # TODO: move this token to Django settings from an environment variable
-    # found in the Mapbox account settings and getting started instructions
-    # see https://www.mapbox.com/account/ under the "Access tokens" section
-    if request.method == 'POST':
-        form = CountriesForm(request.POST)
-        if form.is_valid():
-            #TODO: make a pin at the location of this country
-            request.session["country"] = form.cleaned_data["country"]
-            return HttpResponseRedirect('/')
-    else:
-        form = CountriesForm()
+    dictionary = json.dumps(DICT)
+
     return render(request, "map/index.html", {
     'mapbox_access_token': mapbox_access_token,
-    'form': form,
-    'country' : request.session["country"],
+    'countries' : sorted(DICT.keys()),
+    'dict' : dictionary,
     'embassy_list': embassy_list,
     })
