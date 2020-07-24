e7ef6176793a58e838fb0269a27c109c02ce3a27
Thong <tho2@gmail.com>
2020-07-23 23:33:55 -0400

    Add CNN web scraping. Currently it is static need to change to take user selected country to search for news on.

--- map/views.py
+++ map/views.py
@@ -3,8 +3,55 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from .forms import CountriesForm
 
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+class NewsArticles():
+    
+    def news():
+        articles = []
+
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+
+        driver = webdriver.Chrome('chromedriver.exe', chrome_options=options)
+        
+        driver.get('https://www.cnn.com/search?q=US%20England%20embassy&size=10&category=world&type=article')
+
+        try:
+                element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME , "cnn-search__result-publish-date")))
+                res = driver.execute_script("return document.getElementsByTagName('html')[0].outerHTML")
+
+                soup = BeautifulSoup(res, 'lxml')
+                box = soup.find('div', {'class': 'cnn-search__results-list'})
+                all_news = box.find_all('div', {'class': 'cnn-search__result cnn-search__result--article'})
+                
+                for new in all_news:
+                        contents = new.find('div', {'class': 'cnn-search__result-contents'})
+                        headline = contents.find('h3', {'class': 'cnn-search__result-headline'})
+                        link = headline.find('a', href=True)['href']
+                        title = headline.find('a').text.replace('\n', '')
+                        
+                        date = contents.find('div', {'class': 'cnn-search__result-publish-date'})
+                        t1 = date.find_all('span')[1].text.replace('\n', '')
+                        
+                        articles = articles + [(title, link, t1)]
+        except TimeoutException:
+                articles = articles + [("Timeout Exception", "", "")]
+        finally:
+                driver.quit()
+
+        return articles
+
 # Create your views here.
 def index(request):
+    list = NewsArticles.news()
     mapbox_access_token = 'pk.my_mapbox_access_token'
 
     if "country" not in request.session:
@@ -24,4 +71,5 @@ def index(request):
     'mapbox_access_token': mapbox_access_token,
     'form': form,
     'country' : request.session["country"],
+    "articles": list,
     })
