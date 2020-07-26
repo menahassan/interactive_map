7d5b1eb6485e8af07d0c0746b08653c36f5f7bee
menahassan <menah9@gmail.com>
2020-07-26 10:50:39 -0700

    add country form code

--- map/urls.py
+++ map/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
