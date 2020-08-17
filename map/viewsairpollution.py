465a1c3f5eec2885e0cc2619c56fbfe0ae5804e8
Elizabeth Sinyavin <esinyavin01@gmail.com>
2020-08-16 23:54:08 -0400

    edits

--- map/viewsairpollution.py
+++ map/viewsairpollution.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from . import embassies_consulates
+from . import DICT
+from . import a
 from . import nodiplpresencelist
 from . import airpollution
 import json
@@ -13,22 +14,15 @@ def index(request):
     issue_dict = {"air_polution": ["Bangladesh", "Pakistan", "Mongolia", "Afghanistan", "India", "Indonesia", "Bahrain"],}
 
     mapbox_access_token = 'pk.my_mapbox_access_token'
-
-    nodiplpresencecountries = []
-    for row in nodiplpresencelist:
-        nodiplpresencecountries.append(row[0])
-
-    diplpresencecountries = []
-    for row in embassies_consulates:
-        diplpresencecountries.append(row[2])
+    embassy_list = json.dumps(a)
+    dictionary = json.dumps(DICT)
 
     return render(request, "map/index.html", {
     'mapbox_access_token': mapbox_access_token,
-    'countries' : sorted(list(set(diplpresencecountries + nodiplpresencecountries))),
-    'embassy_list': json.dumps(embassies_consulates),
+    'countries' : sorted(DICT.keys()),
+    'embassy_list': embassy_list,
+    'dict' : dictionary,
     'issuedict' : json.dumps(issue_dict),
     'nodiplpresencelist': nodiplpresencelist,
     'airpollution': airpollution,
     })
-
-#^i used this to find set of countries not among the list in the original excel w/ embassies
