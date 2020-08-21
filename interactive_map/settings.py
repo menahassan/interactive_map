dafd3b3801ab08253ea91915b0416da5c59ccf80
esinyavin <68335679+esinyavin@users.noreply.github.com>
2020-08-21 19:25:46 -0400

    Update settings.py

--- interactive_map/settings.py
+++ interactive_map/settings.py
@@ -135,3 +135,12 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
