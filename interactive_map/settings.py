b257d67a1176788ef6098fe66883de04fe05f874
esinyavin <68335679+esinyavin@users.noreply.github.com>
2020-08-21 18:59:43 -0400

    Update settings.py

--- interactive_map/settings.py
+++ interactive_map/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$u3$ma3v3ydv1+&&pm-ep$m&2cr=m79ubdk5nn)^91ov631km2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
