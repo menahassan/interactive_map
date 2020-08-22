27cf35e4ea0df28297d69ebf71ebc5d2de4a6afd
esinyavin <68335679+esinyavin@users.noreply.github.com>
2020-08-21 22:22:41 -0400

    Update views.py

--- map/views.py
+++ map/views.py
@@ -18,8 +18,6 @@ import json
 
 def index(request):
     #TO DO: automize this
-    issue_dict = {"air_polution": ["Bangladesh", "Pakistan", "Mongolia", "Afghanistan", "India", "Indonesia", "Bahrain"],}
-
     mapbox_access_token = 'pk.my_mapbox_access_token'
 
     nodiplpresencecountries = []
@@ -34,7 +32,7 @@ def index(request):
     for row in issues_summaries:
         issues.append(row[0])
 
-    lang_list = json.dumps(translated)
+    #lang_list = json.dumps(translated)
     return render(request, "map/index.html", {
     'mapbox_access_token': mapbox_access_token,
     'countries' : sorted(list(set(diplpresencecountries + nodiplpresencecountries))),
@@ -48,14 +46,14 @@ def index(request):
     })
 
 
-def hdiMap(request):
-    hdi = json.dumps(countriesHDI)
-    return render(request, "map/hdiMap.html", {
-    'countriesHDI': hdi,
-    })
+#def hdiMap(request):
+#    hdi = json.dumps(countriesHDI)
+#    return render(request, "map/hdiMap.html", {
+#    'countriesHDI': hdi,
+#    })
 
-def embassyYearOpen(request):
-    yearOpen = json.dumps(DICT)
-    return render(request, "map/embassyYearOpen.html", {
-    'embassiesData': yearOpen,
-    })
+#def embassyYearOpen(request):
+#    yearOpen = json.dumps(DICT)
+#    return render(request, "map/embassyYearOpen.html", {
+#    'embassiesData': yearOpen,
+#    })
