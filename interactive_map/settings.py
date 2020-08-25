c88938f7c2af7fadc58131a9a74254a8b3e5347b
menahassan <menah9@gmail.com>
2020-08-25 12:05:58 -0700

    added data namespace

--- interactive_map/settings.py
+++ interactive_map/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ["shielded-ocean-82859.herokuapp.com","127.0.0.1"]
 
 INSTALLED_APPS = [
     'map',
+    'data',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- interactive_map/urls.py
+++ interactive_map/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('map/', include('map.urls')),
     path('', include('map.urls')),
+    url(r'data/', include('data.urls', namespace='data')),
 ]
