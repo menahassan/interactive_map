ceffc8933c5dcce9d03a0304df643fc531151f60
menahassan <menah9@gmail.com>
2020-08-25 12:16:41 -0700

    update urls.py

--- interactive_map/urls.py
+++ interactive_map/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path, url
+from django.urls import include, path
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('map/', include('map.urls')),
     path('', include('map.urls')),
+    url(r'^map/', include('map.urls', namespace='map')),
     url(r'data/', include('data.urls', namespace='data')),
 ]
